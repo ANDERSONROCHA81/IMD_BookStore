eeb0060d543d05cf466aeddbf190715e44faa983
andersonrocha <ironmanrocha81@gmail.com>
2025-01-02 01:10:37 -0300

    Implementação da lógica dos botões das telas de cadastro, login e trocar senha

--- app/src/main/java/com/example/imdbookstore/TrocarSenhaActivity.kt
+++ app/src/main/java/com/example/imdbookstore/TrocarSenhaActivity.kt
@@ -1,20 +1,24 @@
 package com.example.imdbookstore
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.imdbookstore.databinding.ActivityTrocarSenhaBinding
 
 class TrocarSenhaActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityTrocarSenhaBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_trocar_senha)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityTrocarSenhaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnMudarSenha.setOnClickListener {
+            val telaLogin = Intent(this, LoginActivity::class.java)
+            startActivity(telaLogin)
         }
     }
 }
