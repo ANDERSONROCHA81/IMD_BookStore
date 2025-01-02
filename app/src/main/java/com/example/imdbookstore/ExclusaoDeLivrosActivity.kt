3381162e8f7512986a24650c6d4e3db2a738db44
andersonrocha <ironmanrocha81@gmail.com>
2025-01-02 18:41:46 -0300

    Criação das activities de exclusão de livros

---
+++ app/src/main/java/com/example/imdbookstore/ExclusaoDeLivrosActivity.kt
@@ -0,0 +1,20 @@
+package com.example.imdbookstore
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ExclusaoDeLivrosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_exclusao_de_livros)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
