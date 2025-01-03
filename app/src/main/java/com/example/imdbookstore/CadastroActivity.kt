52a826f6b0323d44f1990b302412f6327d2b7866
andersonrocha <ironmanrocha81@gmail.com>
2025-01-02 21:08:49 -0300

    Implementaçao da lógica de cadastro de usuário e de login

--- app/src/main/java/com/example/imdbookstore/CadastroActivity.kt
+++ app/src/main/java/com/example/imdbookstore/CadastroActivity.kt
@@ -1,7 +1,9 @@
 package com.example.imdbookstore
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.imdbookstore.databinding.ActivityCadastroBinding
@@ -17,8 +19,24 @@ class CadastroActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btnCriarConta.setOnClickListener {
-            val telaLogin = Intent(this, LoginActivity::class.java)
-            startActivity(telaLogin)
+            var nome = binding.etNome.text.toString()
+            var senha = binding.etSenha.text.toString()
+
+            if (nome.isNotEmpty() && senha.isNotEmpty()) {
+                salvarUsuario(this, senha, nome)
+                Toast.makeText(this, "Cadastro feito com sucesso", Toast.LENGTH_LONG).show()
+                val telaLogin = Intent(this, LoginActivity::class.java)
+                startActivity(telaLogin)
+            } else {
+                Toast.makeText(this, "Preencha todos os campos", Toast.LENGTH_LONG).show()
+            }
         }
+    }
+
+    fun salvarUsuario(context: Context, senha: String, nomeUsuario: String) {
+        val sharedPreferences = context.getSharedPreferences("Usuarios", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putString(senha, nomeUsuario)
+        editor.apply()
     }
 }
--- app/src/main/java/com/example/imdbookstore/LoginActivity.kt
+++ app/src/main/java/com/example/imdbookstore/LoginActivity.kt
@@ -1,7 +1,9 @@
 package com.example.imdbookstore
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.imdbookstore.databinding.ActivityLoginBinding
@@ -17,8 +19,21 @@ class LoginActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btnLogin.setOnClickListener {
-            val telaMenu = Intent(this, MenuActivity::class.java)
-            startActivity(telaMenu)
+            var nome = binding.etNome.text.toString()
+            var senha = binding.etSenha.text.toString()
+            var usuario = recuperarUsuario(this, senha)
+
+            if (nome.isNotEmpty() && senha.isNotEmpty()) {
+                if (nome == usuario) {
+                    Toast.makeText(this, "Usuário logado com sucesso", Toast.LENGTH_LONG).show()
+                    val telaMenu = Intent(this, MenuActivity::class.java)
+                    startActivity(telaMenu)
+                }else{
+                    Toast.makeText(this, "Usuário não encontrado", Toast.LENGTH_LONG).show()
+                }
+            } else {
+                Toast.makeText(this, "Informe o nome e a senha", Toast.LENGTH_LONG).show()
+            }
         }
 
         binding.tvEsqueceuSenha.setOnClickListener {
@@ -30,5 +45,11 @@ class LoginActivity : AppCompatActivity() {
             val telaCadastro = Intent(this, CadastroActivity::class.java)
             startActivity(telaCadastro)
         }
+    }
+
+    fun recuperarUsuario(context: Context, senha: String): String {
+        val sheredPreferences = context.getSharedPreferences("Usuarios", Context.MODE_PRIVATE)
+        val usuario = sheredPreferences.getString(senha, null)
+        return usuario.toString()
     }
 }
